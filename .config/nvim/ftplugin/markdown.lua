fdf8a630220b9d183a9227663388c1c681cb38eb
orange <orange@unknowncabd84aba12c.attlocal.net>
2025-06-08 14:37:22 -0500

    bah

--- .config/nvim/ftplugin/markdown.lua
+++ .config/nvim/ftplugin/markdown.lua
@@ -1,2 +1,2 @@
-vim.api.nvim_set_keymap('n', 'gd', ':ObsidianFollowLink<cr>', {noremap=true, silent=true})
-vim.api.nvim_set_keymap('n', '<leader>c', ':ObsidianNew', {noremap=true, silent=true})
+-- vim.api.nvim_set_keymap('n', 'gd', ':ObsidianFollowLink<cr>', {noremap=true, silent=true})
+-- vim.api.nvim_set_keymap('n', '<leader>c', ':ObsidianNew', {noremap=true, silent=true})
--- README.md
+++ README.md
@@ -38,3 +38,4 @@ reliably use either that often anymore
 - Setup script?
 - Eventually learn [Nix](https://nixos.org/)
 - Fish config? (I don't really maintain it between machines at the moment)
+- Neovim configuration cleanup
